77416d99b9678eb6ff7245ea8c40fb6aaa0200a9
Armando Cortes <ajcortest@outlook.com>
2025-11-01 09:39:51 -0600

    fix: Usar @ts-expect-error en lugar de type assertions para globalForPrisma

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,16 +4,16 @@ import { PrismaClient } from '@prisma/client'
 // exhausting your database connection limit.
 // Learn more: https://pris.ly/d/help/next-js-best-practices
 
-const globalForPrisma: {
-  prisma?: PrismaClient | undefined
-} = globalThis
+const globalForPrisma = globalThis
 
+// @ts-expect-error - globalThis prisma property added dynamically
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   })
 
+// @ts-expect-error - globalThis prisma property added dynamically
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export * from '@prisma/client'
